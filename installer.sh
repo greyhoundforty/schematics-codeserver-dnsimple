cac99803ad84f7f4b837af1ccc6f611ce947a9f2
greyhoundforty <ryantiffany@fastmail.com>
2020-07-04 17:30:26 -0500

    Inital commit
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

---
+++ installer.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash 
+
+## Update machine
+DEBIAN_FRONTEND=noninteractive apt -qqy update
+DEBIAN_FRONTEND=noninteractive apt-get -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade
+
+## Install Code Server
+curl -fOL "https://github.com/cdr/code-server/releases/download/v${release}/code-server_${release}_amd64.deb"
+sudo dpkg -i "code-server_${release}_amd64.deb"
+systemctl --user enable --now code-server
+
+## Install Caddy
+echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" | tee -a /etc/apt/sources.list.d/caddy-fury.list
+DEBIAN_FRONTEND=noninteractive apt -qqy update
+DEBIAN_FRONTEND=noninteractive apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install caddy
+
+## Backup original caddyfile
+mv /etc/caddy/Caddyfile /root/Caddyfile-original
+
+## Generate new Caddyfile 
+cat >/etc/caddy/Caddyfile <<EOL
+${fqdn}
+reverse_proxy 127.0.0.1:8080
+EOL
+
+## Set permissions and ownership on new Caddyfile
+chmod 0644 /etc/caddy/Caddyfile
+chown root:root /etc/caddy/Caddyfile
+
+## Restart Caddy so Let's Encrypt can do it's thing
+systemctl reload caddy
---
+++ providers.tf
@@ -0,0 +1,9 @@
+provider "ibm" {
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
+}
+
+provider "dnsimple" {
+  token   = var.dnsimple_token
+  account = var.dnsimple_account
+}
